aaa66eaeae0b02fb440376773dd4228505b3e1d5
Tobias Streng <tobias.streng@gmx.de>
2020-03-16 10:52:56 +0100

    :book: Add Description

--- src/DateTimeRange/DateTimeExtensions.cs
+++ src/DateTimeRange/DateTimeExtensions.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace System
+﻿namespace System
 {
     public static class DateTimeExtensions
     {
-        public static bool IsInRange(this DateTime dateTime, DateTimeRange dateTimeRange)
+        /// <summary>
+        /// Checks if the <see cref="DateTime"/> is inside of the <see cref="DateTimeRange"/>.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <param name="dateTimeRange"></param>
+        /// <returns></returns>
+        public static bool InRange(this DateTime dateTime, DateTimeRange dateTimeRange)
         => dateTime >= dateTimeRange.Start && dateTime <= dateTimeRange.End;
 
     }
--- src/DateTimeRange/DateTimeRange.cs
+++ src/DateTimeRange/DateTimeRange.cs
@@ -1,27 +1,56 @@
 ﻿namespace System
 {
-    public struct DateTimeRange
+    public struct DateTimeRange : IEquatable<DateTimeRange>
     {
+        /// <summary>
+        /// The <see cref="DateTime"/>, the range starts with
+        /// </summary>
         public DateTime Start { get; }
+
+        /// <summary>
+        /// The <see cref="DateTime"/>, the range ends with
+        /// </summary>
         public DateTime End { get; }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="DateTimeRange"/>
+        /// </summary>
+        /// <param name="startDate"><see cref="DateTime"/> the range starts with</param>
+        /// <param name="endDate"><see cref="DateTime"/> the range ends with</param>
         public DateTimeRange(DateTime startDate, DateTime endDate)
         {
-            if (startDate >= endDate) throw new DateTimeInvalidRangeException("Start date is later than end date or equal.");
+            if (startDate > endDate) throw new DateTimeInvalidRangeException("Start date is later than end date");
             Start = startDate;
             End = endDate;
         }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="DateTimeRange"/>
+        /// </summary>
+        /// <param name="startDate"><see cref="DateTime"/> the range starts with</param>
+        /// <param name="duration">Duration from StartDate to EndDate</param>
         public DateTimeRange(DateTime startDate, TimeSpan duration)
         {
+            if (duration.TotalMilliseconds < 0) throw new DateTimeInvalidRangeException("Duration is negative");
             Start = startDate;
             End = startDate.Add(duration);
         }
-        
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="DateTimeRange"/>
+        /// </summary>
+        /// <param name="duration">Duration from StartDate to EndDate</param>
+        /// <param name="endDate"><see cref="DateTime"/> the range ends with</param>
         public DateTimeRange(TimeSpan duration, DateTime endDate)
         {
+            if (duration.TotalMilliseconds < 0) throw new DateTimeInvalidRangeException("Duration is negative");
             Start = endDate.Add(-duration);
             End = endDate;
+        }
+
+        public bool Equals(DateTimeRange other)
+        {
+            return this.Start == other.Start && this.End == other.End;
         }
     }
 }
